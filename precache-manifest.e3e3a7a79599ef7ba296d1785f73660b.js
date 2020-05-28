1cf32894c522979709beff7a37d21a64be7011dd
manjunathvustepalli <manjunathvustepalli@users.noreply.github.com>
2020-05-28 20:13:10 +0530

    Updates

---
+++ precache-manifest.e3e3a7a79599ef7ba296d1785f73660b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9532a94a59411088ee6cae5b6f068945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02625fac7fa3271639bd",
+    "url": "/static/css/main.ca1d9e8d.chunk.css"
+  },
+  {
+    "revision": "c5dda9b6a18dad886b89",
+    "url": "/static/js/2.2e75c70e.chunk.js"
+  },
+  {
+    "revision": "9c9faccec3ea673a1fc84b7d94939a73",
+    "url": "/static/js/2.2e75c70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02625fac7fa3271639bd",
+    "url": "/static/js/main.6388f5d0.chunk.js"
+  },
+  {
+    "revision": "ad8caf16babc2df44569",
+    "url": "/static/js/runtime-main.e65a3091.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
