cb1efda495fdc6004f03c2744167516b69e0a854
manjunathvustepalli <manjunathvustepalli@users.noreply.github.com>
2020-05-29 13:29:19 +0530

    Updates

---
+++ precache-manifest.e69525ef707fbd1a4c53d35a9c6d26d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6864a35305b00f91d95e4178d9af149f",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "ebd522d8fe5c1bad1b20",
+    "url": "/myportfolio/static/css/main.fb05e561.chunk.css"
+  },
+  {
+    "revision": "c5dda9b6a18dad886b89",
+    "url": "/myportfolio/static/js/2.2e75c70e.chunk.js"
+  },
+  {
+    "revision": "9c9faccec3ea673a1fc84b7d94939a73",
+    "url": "/myportfolio/static/js/2.2e75c70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd522d8fe5c1bad1b20",
+    "url": "/myportfolio/static/js/main.78e81a7f.chunk.js"
+  },
+  {
+    "revision": "331faa8402dd0f6ae6f6",
+    "url": "/myportfolio/static/js/runtime-main.96a916e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.54d39289cd43b098c58519ccfd4e30de.js"
+  "/myportfolio/precache-manifest.e69525ef707fbd1a4c53d35a9c6d26d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
